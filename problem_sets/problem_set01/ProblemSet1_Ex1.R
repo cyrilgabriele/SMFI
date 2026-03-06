6fd1c3b099d5fced3562522b62fad81cab9ad8a5
Cyril Gabriele <cyril.gabriele@gmail.com>
2026-03-06 07:14:29 +0100

    finished the comments

--- problem_sets/problem_set01/ProblemSet1_Ex1.R
+++ problem_sets/problem_set01/ProblemSet1_Ex1.R
@@ -35,6 +35,8 @@ plot(tgrid, St, type = "l", col = "darkgreen", lwd = 2,
      xlab = "Time t", ylab = "Price S_t", main = "Corresponding price path")
 abline(h = mean(St), col = "black", lwd = 2)
 
+par(mfrow = c(1, 1))   # reset so subsequent plots are standalone
+
 # ============================================================
 # (b) mu_hat from the single path
 # ============================================================
@@ -71,22 +73,29 @@ T       <- 3
 N_paths <- 100000
 
 W_T <- rnorm(N_paths, mean = 0, sd = 1)  
-# approximation formula *1)
-S_T <- S0 * exp((mu - 0.5 * sigma^2) * T +     # GBM solution at T
+# approximation formula *1) (see below)
+S_T <- S0 * exp((mu - 0.5 * sigma^2) * T +     # GBM approximation formula
                 sigma * sqrt(T) * W_T)
 
 hist(S_T, breaks = 60, col = "lightblue", border = "white",
+     freq = FALSE,
      main = "GBM terminal prices S_T (T = 3)",
      xlab = expression(S[T]))
 
+mu_log    <- log(S0) + (mu - 0.5 * sigma^2) * T
+sigma_log <- sigma * sqrt(T)
+curve(dlnorm(x, meanlog = mu_log, sdlog = sigma_log), add = TRUE,
+      col = "darkred", lwd = 2)
+
 S_T_sample_mean <- mean(S_T)
 S_T_theoretical <- S0 * exp(mu * T)
-S_T_abs_mean_diff   <- abs(mean(S_T_sample_mean - S_T_theoretical))
+S_T_mean_diff      <- S_T_sample_mean - S_T_theoretical
+S_T_abs_mean_diff  <- abs(S_T_mean_diff)
 # Comment: 
 # Shape: the histogram indicates a log normal distribution.
 # This since W_T is sampled from a normal dist. and then taken exponentially in
-# the approximation formula *1). This results in a log-normal dist.
+# the approximation formula *1). This results in a log-normal dist. See also the PDF
+# as a black curve with the parameters implied by the model.
 # Obviously we expect a (small) difference between the analytical closed-form 
 # solution and the Monte Carlo approixmation approach. The abs diff of ~0.07 
 # is in my opinion small w.r.t to the total value of ~127. 
-
